ebf64e50f3de4b7e77e8b88a31c39ccc0e41c735
Tisawesomeness <creeperscreep.tk@gmail.com>
2017-04-26 15:40:38 -0400

    Added support for user IDs to define the user to DM

--- src/com/tisawesomeness/minecord/command/admin/MsgCommand.java
+++ src/com/tisawesomeness/minecord/command/admin/MsgCommand.java
@@ -1,9 +1,9 @@
 package com.tisawesomeness.minecord.command.admin;
 
 import java.util.concurrent.ExecutionException;
-import java.util.regex.Pattern;
+import org.apache.commons.lang3.ArrayUtils;
 
-import com.tisawesomeness.minecord.Config;
+import com.tisawesomeness.minecord.Bot;
 import com.tisawesomeness.minecord.command.Command;
 
 import net.dv8tion.jda.core.entities.PrivateChannel;
@@ -28,7 +28,8 @@ public class MsgCommand extends Command {
 		);
 	}
 	
-	private final String regex = "[a-zA-Z]+ <@!?[0-9]+> ";
+	private final String mentionRegex = "<@!?[0-9]+> ";
+	private final String idRegex = "[0-9]{18}";
 	
 	public Result run(String[] args, MessageReceivedEvent e) {
 		
@@ -37,11 +38,20 @@ public class MsgCommand extends Command {
 			return new Result(Outcome.WARNING, ":warning: Please specify a message.");
 		}
 		
+		String param = e.getMessage().getRawContent().split(" ")[0];
+		User user = null;
+		if (param.matches(mentionRegex)) {
+			user = e.getMessage().getMentionedUsers().get(0);
+		} else if (param.matches(idRegex)) {
+			user = Bot.jda.getUserById(param);
+		} else {
+			return new Result(Outcome.ERROR, ":x: Not a valid user!");
+		}
+		
 		//Send the message
-		User user = e.getMessage().getMentionedUsers().get(0);
 		try {
 			PrivateChannel channel = user.openPrivateChannel().submit().get();
-			String msg = e.getMessage().getRawContent().replaceFirst(Pattern.quote(Config.getPrefix()) + regex, "");
+			String msg = String.join(" ", ArrayUtils.remove(args, 0));
 			channel.sendMessage(msg).queue();
 		} catch (InterruptedException | ExecutionException ex) {
 			ex.printStackTrace();
