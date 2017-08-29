8ff2caa781c48a3b6089c7723cc5588b434b70d1
lordjbs <stebojo262@gmail.com>
2017-08-29 11:12:15 +0200

    BETA: Added Exceptions Added A logging system (not implemented in main bot code yet but soon(TM)) Added StringHolder - An easy permanent String array (ALPHA) Added ExceptionListSystem - Lists Exceptions happend earlier. Will add command soon.

--- src/io/github/lordjbs/minecord/java/StringHolder.java
+++ src/io/github/lordjbs/minecord/java/StringHolder.java
@@ -16,40 +16,115 @@ public class StringHolder {
     private String[] val = STANDARD_VALUE;
     public String version = "ALPHA_1";
 
+    /**
+     * Just set the normal Stringholder. Mainly for using .set/.add afterwards.
+     */
     public StringHolder() {
         this.val = STANDARD_VALUE;
     }
 
+    /**
+     * Make new StringHolder with already an existing String[].
+     * @param values A base string[]. You can also use the Stringholder without args.
+     */
     public StringHolder(String[] values) {
         this.val = values;
     }
 
-    public void set(String[] newval) {
+    /**
+     * Set the old to a complete new String[].
+     * @param newValue The new String[] value.
+     */
+    public void setComplete(String[] newValue) {
         try {
-            this.val = newval;
+            this.val = newValue;
         }catch(NotStringException ex) {
             ex.printStackTrace();
             ExceptionListSystem.addException(ex.getMessage());
         }
     }
 
-    public void set(int index, String valtoadd) {
-        this.val[index] = valtoadd;
+    /**
+     * Set the String[index] to a new value.
+     * @param index The index of the String[] that should be set.
+     * @param valueToAdd The value to add.
+     */
+    public void set(int index, String valueToAdd) {
+        this.val[index] = valueToAdd;
+    }
+
+    /**
+     * Adds a String at the next not used position.
+     * @param str The String to add.
+     */
+    public void add(String str) {
+        this.val[(val.length) + 1] = str;
+    }
+
+    /**
+     * Gets the full StringHolder as String[].
+     * @return String[]
+     */
+    public String[] get() {
+        return this.val;
+    }
+
+    /**
+     * Get a value from the choosen index.
+     * @param index The index.
+     * @return String
+     */
+    public String get(int index) {
+        return this.val[index];
+    }
+
+    /**
+     * Get all strings in one String.
+     * @return String
+     */
+    public String getValueParsed() {
+        return Arrays.stream(this.val).collect(Collectors.joining(" "));
+    }
+
+
+    /**
+     * Removes the number from index given, and sorts.
+     * @param index index to remove-
+     */
+    public void remove(int index)  {
+        String[] oldval = val;
+        String[] newval = {};
+        int currentindex = 0;
+        for (String s : oldval) {
+            if(currentindex != index) {
+                newval[currentindex] = s;
+            }
+            currentindex++;
+        }
+    }
+
+    /**
+     * Search value and remove it. It sorts after that.
+     * @param name the value to remove
+     */
+    public void remove(String name)  {
+        String[] oldval = val;
+        String[] newval = {};
+        int currentindex = 0;
+        for (String s : oldval) {
+            if(!oldval[currentindex].equals(name)) {
+                newval[currentindex] = s;
+            }
+            currentindex++;
+        }
     }
 
     public void clear() {
         this.val = new String[]{};
     }
 
-    public String[] get() {
-        return this.val;
-    }
 
-    public String get(int index) {
-        return this.val[index];
-    }
-
-    public String getValueParsed() {
-        return Arrays.stream(this.val).collect(Collectors.joining(" "));
+    public void removeAll() {
+        this.val = new String[]{};
     }
 }
