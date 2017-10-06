51bd930c00410ad74594b3acb529a1b4293e8bff
Tisawesomeness <creeperscreep.tk@gmail.com>
2017-10-06 12:47:06 -0400

    Added some sample recipes

--- src/com/tisawesomeness/minecord/item/Version.java
+++ src/com/tisawesomeness/minecord/item/Version.java
@@ -1,7 +1,12 @@
 package com.tisawesomeness.minecord.item;
 
 public enum Version {
-	V1_8("1.8"), V1_9("1.9"), V1_11("1.11");
+	V1_8("Since 1.8"),
+	VBEFORE_1_9("Before 1.9"),
+	V1_9("Since 1.9"),
+	V1_10("Since 1.10"),
+	V1_11("Since 1.11"),
+	V1_12("Since 1.12");
 	
 	private String s;
 	private Version(String s) {
