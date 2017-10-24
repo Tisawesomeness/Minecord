88dd5c7ba1eace06ef8b1b63e6c661d32cb9251f
Tisawesomeness <creeperscreep.tk@gmail.com>
2017-10-24 15:25:02 -0400

    Simplified recipe enum, added items and recipes up to ID 100

--- src/com/tisawesomeness/minecord/item/Recipe.java
+++ src/com/tisawesomeness/minecord/item/Recipe.java
@@ -2,36 +2,40 @@ package com.tisawesomeness.minecord.item;
 
 public enum Recipe {
 	
+	//0
 	STONE(RecipeType.SMELTING),
 	GRANITE(RecipeType.SHAPELESS, Version.V1_8),
-	POLISHED_GRANITE(RecipeType.CRAFTING, Version.V1_8),
+	POLISHED_GRANITE(Version.V1_8),
 	DIORITE(RecipeType.CRAFTING, Version.V1_8),
-	POLISHED_DIORITE(RecipeType.CRAFTING, Version.V1_8),
+	POLISHED_DIORITE(Version.V1_8),
 	ANDESITE(RecipeType.SHAPELESS, Version.V1_8),
-	POLISHED_ANDESITE(RecipeType.CRAFTING, Version.V1_8),
-	OAK_PLANKS(RecipeType.CRAFTING),
-	SPRUCE_PLANKS(RecipeType.CRAFTING),
-	BIRCH_PLANKS(RecipeType.CRAFTING),
-	JUNGLE_PLANKS(RecipeType.CRAFTING),
-	ACACIA_PLANKS(RecipeType.CRAFTING),
-	DARK_OAK_PLANKS(RecipeType.CRAFTING),
+	POLISHED_ANDESITE(Version.V1_8),
+	OAK_PLANKS(),
+	SPRUCE_PLANKS(),
+	BIRCH_PLANKS(),
+	JUNGLE_PLANKS(),
+	ACACIA_PLANKS(),
+	DARK_OAK_PLANKS(),
 	
+	//10
 	SPONGE(RecipeType.SMELTING),
 	
+	//20
 	GLASS(RecipeType.SMELTING),
-	LAPIS_LAZULI_BLOCK(RecipeType.CRAFTING),
-	DISPENSER(RecipeType.CRAFTING, "The bow must have full durability."),
-	SANDSTONE(RecipeType.CRAFTING),
-	CHISELED_SANDSTONE(RecipeType.CRAFTING),
-	SMOOTH_SANDSTONE(RecipeType.CRAFTING),
-	NOTE_BLOCK(RecipeType.CRAFTING),
+	LAPIS_LAZULI_BLOCK(),
+	DISPENSER("The bow must have full durability."),
+	SANDSTONE(),
+	CHISELED_SANDSTONE(),
+	SMOOTH_SANDSTONE(),
+	NOTE_BLOCK(),
 	//If you can get the white colored bed icon, please send me it
-	BED(RecipeType.CRAFTING_SHAPELESS, "From 1.12 onwards, use any matching wool or dye to color the bed.", true),
-	POWERED_RAIL(RecipeType.CRAFTING),
-	DETECTOR_RAIL(RecipeType.CRAFTING),
-	STICKY_PISTON(RecipeType.CRAFTING),
+	BED(RecipeType.CRAFTING_SHAPELESS, "From 1.12 onwards, use any matching glass or dye to color the bed.", true),
+	POWERED_RAIL(),
+	DETECTOR_RAIL(),
+	STICKY_PISTON(),
 	
-	PISTON(RecipeType.CRAFTING),
+	//30
+	PISTON(),
 	WOOL(RecipeType.CRAFTING_SHAPELESS, "Combine any wool with any dye to change its color.", true),
 	ORANGE_WOOL(RecipeType.SHAPELESS, "Combine any wool with any dye to change its color."),
 	MAGENTA_WOOL(RecipeType.SHAPELESS, "Combine any wool with any dye to change its color."),
@@ -49,21 +53,71 @@ public enum Recipe {
 	RED_WOOL(RecipeType.SHAPELESS, "Combine any wool with any dye to change its color."),
 	BLACK_WOOL(RecipeType.SHAPELESS, "Combine any wool with any dye to change its color."),
 	
-	GOLD_BLOCK(RecipeType.CRAFTING),
-	IRON_BLOCK(RecipeType.CRAFTING),
-	STONE_SLAB(RecipeType.CRAFTING),
-	SANDSTONE_SLAB(RecipeType.CRAFTING),
-	WOODEN_SLAB(RecipeType.CRAFTING),
-	COBBLESTONE_SLAB(RecipeType.CRAFTING),
-	BRICK_SLAB(RecipeType.CRAFTING),
-	STONE_BRICK_SLAB(RecipeType.CRAFTING, "Any stone brick variant is allowed."),
-	NETHER_BRICK_SLAB(RecipeType.CRAFTING),
-	QUARTZ_SLAB(RecipeType.CRAFTING),
-	BRICKS(RecipeType.CRAFTING),
-	TNT(RecipeType.CRAFTING),
-	BOOKSHELF(RecipeType.CRAFTING),
+	//40
+	GOLD_BLOCK(),
+	IRON_BLOCK(),
+	STONE_SLAB(),
+	SANDSTONE_SLAB(),
+	WOODEN_SLAB(),
+	COBBLESTONE_SLAB(),
+	BRICK_SLAB(),
+	STONE_BRICK_SLAB("Any stone brick variant is allowed."),
+	NETHER_BRICK_SLAB(),
+	QUARTZ_SLAB(),
+	BRICKS(),
+	TNT(),
+	BOOKSHELF(),
 	
-	TORCH(RecipeType.CRAFTING, true);
+	//50
+	TORCH(true),
+	OAK_WOOD_STAIRS(),
+	CHEST(),
+	DIAMOND_BLOCK(),
+	CRAFTING_TABLE(),
+	
+	//60
+	FURNACE(),
+	LADDER(),
+	RAIL(),
+	COBBLESTONE_STAIRS(),
+	LEVER(),
+	
+	//70
+	STONE_PRESSURE_PLATE(),
+	WOODEN_PRESSURE_PLATE(),
+	REDSTONE_TORCH(),
+	STONE_BUTTON(),
+	
+	//80
+	SNOW_BLOCK(),
+	CLAY_BLOCK(),
+	JUKEBOX(),
+	OAK_FENCE(),
+	GLOWSTONE(),
+	
+	//90
+	JACK_O_LANTERN(),
+	STAINED_GLASS("Combine any glass with any dye to change its color."),
+	ORANGE_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	MAGENTA_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	LIGHT_BLUE_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	YELLOW_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	LIME_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	PINK_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	GRAY_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	LIGHT_GRAY_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	CYAN_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	PURPLE_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	BLUE_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	BROWN_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	GREEN_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	RED_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	BLACK_STAINED_GLASS("Combine any glass with any dye to change its color."),
+	WOODEN_TRAPDOOR(),
+	STONE_BRICKS(),
+	MOSSY_STONE_BRICKS(),
+	CRACKED_STONE_BRICKS(),
+	CHISELED_STONE_BRICKS();
 	
 	public final Item item;
 	public final RecipeType type;
@@ -71,8 +125,20 @@ public enum Recipe {
 	public final String notes;
 	public final boolean gif;
 	
+	private Recipe() {
+		this(RecipeType.CRAFTING, null, null, false);
+	}
 	private Recipe(RecipeType type) {
 		this(type, null, null, false);
+	}
+	private Recipe(Version version) {
+		this(RecipeType.CRAFTING, version, null, false);
+	}
+	private Recipe(String notes) {
+		this(RecipeType.CRAFTING, null, notes, false);
+	}
+	private Recipe(boolean gif) {
+		this(RecipeType.CRAFTING, null, null, true);
 	}
 	private Recipe(RecipeType type, Version version) {
 		this(type, version, null, false);
