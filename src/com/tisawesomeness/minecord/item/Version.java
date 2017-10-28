34873f683ef6a54d110c42dcb3f693d60b9348e6
Tisawesomeness <creeperscreep.tk@gmail.com>
2017-10-27 23:20:00 -0400

    Added item regex and version up to ID 255

--- src/com/tisawesomeness/minecord/item/Version.java
+++ src/com/tisawesomeness/minecord/item/Version.java
@@ -5,6 +5,7 @@ public enum Version {
 	VBEFORE_1_9("Before 1.9"),
 	V1_9("Since 1.9"),
 	V1_10("Since 1.10"),
+	V1_9_USE_1_10("Since 1.9, Usable since 1.10"),
 	V1_11("Since 1.11"),
 	V1_12("Since 1.12");
 	
