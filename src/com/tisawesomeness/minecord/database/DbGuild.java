a321e62321b3b6adee83f6b491a267b5b276bec9
Tisawesomeness <tis@creeperscreep.com>
2018-02-14 13:43:11 -0500

    Added lang column in guild table

--- src/com/tisawesomeness/minecord/database/DbGuild.java
+++ src/com/tisawesomeness/minecord/database/DbGuild.java
@@ -4,12 +4,14 @@ public class DbGuild {
 	
 	public long id;
 	public String prefix;
+	public String lang;
 	public boolean banned;
 	public boolean noCooldown;
 	
-	public DbGuild(long id, String prefix, boolean banned, boolean noCooldown) {
+	public DbGuild(long id, String prefix, String lang, boolean banned, boolean noCooldown) {
 		this.id = id;
 		this.prefix = prefix;
+		this.lang = lang;
 		this.banned = banned;
 		this.noCooldown = noCooldown;
 	}
