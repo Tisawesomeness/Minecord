a118bf5b747611545c34f3db088d3a486e5fd717
Tisawesomeness <tis@creeperscreep.com>
2018-04-20 10:58:34 -0400

    Made &codes simpler, moved old images to an argument

--- src/com/tisawesomeness/minecord/command/utility/CodesCommand.java
+++ src/com/tisawesomeness/minecord/command/utility/CodesCommand.java
@@ -2,15 +2,18 @@ package com.tisawesomeness.minecord.command.utility;
 
 import com.tisawesomeness.minecord.command.Command;
 
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public class CodesCommand extends Command {
+	
+	private String img = "https://minecraft.gamepedia.com/media/minecraft.gamepedia.com/7/7e/Minecraft_Formatting.gif";
 	
 	public CommandInfo getInfo() {
 		return new CommandInfo(
 			"codes",
 			"Lists the available chat codes.",
-			null,
+			"[detailed?]",
 			new String[]{
 				"code",
 				"chat",
@@ -25,13 +28,16 @@ public class CodesCommand extends Command {
 	
 	public Result run(String[] args, MessageReceivedEvent e) {
 		
-		String m = "**Chat Codes:**" +
+		if (args.length > 0) {
+			return new Result(Outcome.SUCCESS, "**Chat Codes:**" +
 				"\n" + "Copy-Paste symbol: `§`" +
 				"\n" + "MOTD code: `\\u00A7`" +
 				"\n" + "Most servers with plugins let you use `&` instead of `§` in chat and config files." +
 				"\n" + "http://i.imgur.com/MWCFs5S.png" +
-				"\n" + "http://i.imgur.com/cWYjhkN.png";
-		return new Result(Outcome.SUCCESS, m);
+				"\n" + "http://i.imgur.com/cWYjhkN.png");
+		} else {
+			return new Result(Outcome.SUCCESS, new EmbedBuilder().setImage(img).build());
+		}
 		
 	}
 
