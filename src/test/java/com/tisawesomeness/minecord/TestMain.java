1a9a1cc172aa9a194a7c46f3ed213698a4a4101a
Tisawesomeness <tis@creeperscreep.com>
2020-06-10 23:13:16 -0400

    Implement testing

---
+++ src/test/java/com/tisawesomeness/minecord/TestMain.java
@@ -0,0 +1,16 @@
+package com.tisawesomeness.minecord;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Tag("Main")
+public class TestMain {
+    @Test
+    @DisplayName("Tests are working")
+    public void testMain() {
+        assertEquals(1, 1);
+    }
+}
