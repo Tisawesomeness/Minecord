cc6dab0670108e7588722a233b5b439dfc7790b1
Tisawesomeness <tis@creeperscreep.com>
2020-06-28 11:57:18 -0400

    Change datatypes in init.sql

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,15 +1,15 @@
-CREATE TABLE IF NOT EXISTS guild (
-    `id` BIGINT(18) NOT NULL,
-    `prefix` TINYTEXT,
-    `lang` TINYTEXT,
+CREATE TABLE IF NOT EXISTS `guild` (
+    `id` BIGINT(20) NOT NULL,
+    `prefix` TEXT(8),
+    `lang` TEXT(5),
     `banned` TINYINT(1) NOT NULL DEFAULT 0,
     `noCooldown` TINYINT(1) NOT NULL DEFAULT 0,
     `deleteCommands` TINYINT(1),
     `noMenu` TINYINT(1),
     PRIMARY KEY (`id`)
 );
-CREATE TABLE IF NOT EXISTS user (
-    `id` BIGINT(18) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+    `id` BIGINT(20) NOT NULL,
     `elevated` TINYINT(1) NOT NULL DEFAULT 0,
     `banned` TINYINT(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`)
