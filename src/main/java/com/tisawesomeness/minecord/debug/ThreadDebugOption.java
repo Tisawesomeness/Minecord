3673a24f1af43ae3322e6c174568416f7dfc6b7a
Tisawesomeness <tis@creeperscreep.com>
2020-06-30 22:23:25 -0400

    Add &debug threads

---
+++ src/main/java/com/tisawesomeness/minecord/debug/ThreadDebugOption.java
@@ -0,0 +1,33 @@
+package com.tisawesomeness.minecord.debug;
+
+import com.tisawesomeness.minecord.util.MessageUtils;
+
+import lombok.NonNull;
+import net.dv8tion.jda.api.utils.MarkdownUtil;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ThreadDebugOption implements DebugOption {
+    public @NonNull String getName() {
+        return "threads";
+    }
+    public @NonNull String debug() {
+        Set<Thread> threads = Thread.getAllStackTraces().keySet();
+        return threads.stream()
+                .map(ThreadDebugOption::getThreadInfo)
+                .collect(Collectors.joining("\n"));
+    }
+
+    private static String getThreadInfo(Thread t) {
+        String threadInfo = String.format("%s %s: ID `%s` | Priority `%s`",
+                t.getState(), MarkdownUtil.bold(t.getName()), t.getId(), t.getPriority());
+        if (t.isDaemon()) {
+            threadInfo += " | DAEMON";
+        }
+        if (Thread.currentThread().equals(t)) {
+            return MarkdownUtil.italics(threadInfo);
+        }
+        return threadInfo;
+    }
+}
