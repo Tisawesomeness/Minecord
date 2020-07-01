69f77f6e99939fb686e7134da9a4f1f99152bf71
Tisawesomeness <tis@creeperscreep.com>
2020-06-30 22:23:09 -0400

    Format &debug console message

--- src/main/java/com/tisawesomeness/minecord/command/admin/DebugCommand.java
+++ src/main/java/com/tisawesomeness/minecord/command/admin/DebugCommand.java
@@ -6,8 +6,10 @@ import com.tisawesomeness.minecord.database.Database;
 import com.tisawesomeness.minecord.debug.DebugOption;
 import com.tisawesomeness.minecord.debug.GuildCacheDebugOption;
 import com.tisawesomeness.minecord.debug.JDADebugOption;
+import com.tisawesomeness.minecord.debug.ThreadDebugOption;
 import com.tisawesomeness.minecord.debug.UserCacheDebugOption;
 
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.sharding.ShardManager;
 
 import java.util.Arrays;
@@ -32,6 +34,7 @@ public class DebugCommand extends Command {
     public DebugCommand(ShardManager sm, Database db) {
         debugOptions = Arrays.asList(
                 new JDADebugOption(sm),
+                new ThreadDebugOption(),
                 new GuildCacheDebugOption(db),
                 new UserCacheDebugOption(db)
         );
@@ -52,7 +55,9 @@ public class DebugCommand extends Command {
         for (DebugOption d : debugOptions) {
             if (d.getName().equalsIgnoreCase(txt.args[0])) {
                 String debugInfo = d.debug();
-                System.out.println(debugInfo); // Useful to have in console
+                User author = txt.e.getAuthor();
+                String requestedBy = String.format("Requested By %s (%s)\n", author.getAsTag(), author.getId());
+                System.out.println("\n" + requestedBy + debugInfo + "\n"); // Useful to have in console
                 return new Result(Outcome.SUCCESS, debugInfo);
             }
         }
