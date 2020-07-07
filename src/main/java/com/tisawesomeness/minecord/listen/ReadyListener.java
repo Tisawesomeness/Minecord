9e282138bf8cac1668bf2498852ce6edb8b8d5a0
Tisawesomeness <tis@creeperscreep.com>
2020-07-06 22:52:49 -0400

    Log number of ready shards instead of how many left

--- src/main/java/com/tisawesomeness/minecord/listen/ReadyListener.java
+++ src/main/java/com/tisawesomeness/minecord/listen/ReadyListener.java
@@ -1,21 +1,26 @@
 package com.tisawesomeness.minecord.listen;
 
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.util.concurrent.CountDownLatch;
 
-@RequiredArgsConstructor
 public class ReadyListener extends ListenerAdapter {
 
     private final @NonNull CountDownLatch readyLatch;
+    private final int shardCount;
+
+    public ReadyListener(CountDownLatch readyLatch) {
+        this.readyLatch = readyLatch;
+        shardCount = (int) readyLatch.getCount();
+    }
 
     @Override
     public void onReady(ReadyEvent e) {
         readyLatch.countDown();
-        System.out.println(readyLatch.getCount() + " shards left.");
+        int readyShards = shardCount - (int) readyLatch.getCount();
+        System.out.printf("%d/%d shards ready%n", readyShards, shardCount);
     }
 
 }
