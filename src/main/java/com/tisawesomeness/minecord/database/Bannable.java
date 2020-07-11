16403bab4e07be9038b66198b6a31812ba2ace01
Tisawesomeness <tis@creeperscreep.com>
2020-07-11 13:19:05 -0400

    Add a little documentation for prev commit

--- src/main/java/com/tisawesomeness/minecord/database/Bannable.java
+++ src/main/java/com/tisawesomeness/minecord/database/Bannable.java
@@ -1,5 +1,11 @@
 package com.tisawesomeness.minecord.database;
 
+/**
+ * Represents something that can be banned from using the bot.
+ */
 public interface Bannable {
+    /**
+     * Whether this is banned.
+     */
     boolean isBanned();
 }
--- src/main/java/com/tisawesomeness/minecord/database/DbObject.java
+++ src/main/java/com/tisawesomeness/minecord/database/DbObject.java
@@ -2,15 +2,19 @@ package com.tisawesomeness.minecord.database;
 
 import java.sql.SQLException;
 
+/**
+ * Mirrors a database table.
+ * <br>This should be an immutable object in order to work well with Guava caches.
+ */
 public interface DbObject {
     /**
-     * The unique ID of this object.
+     * The unique, unchangeable ID of this object.
      * <br>For Discord IDs, this is 17 to 20 digits long.
      * @return A positive number
      */
     long getId();
     /**
-     * Updates or inserts this object into the database.
+     * Updates or inserts this object into the database, using {@link #getId()} as the {@code id} column.
      * @throws SQLException If a database error occurs
      */
     void update() throws SQLException;
