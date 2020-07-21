9d7efc32264d87f79ee0f356ec267c2b14714669
Tisawesomeness <tis@creeperscreep.com>
2020-07-21 14:37:42 -0400

    Add package-info.java to parse package

---
+++ src/main/java/com/tisawesomeness/minecord/setting/parse/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains classes that parses arguments for and executes the
+ * {@code &settings}, {@code &set}, and {@code &reset} commands.
+ * <br>The entry point is {@link com.tisawesomeness.minecord.setting.parse.SettingCommandParser},
+ * and the command type is defined in {@link com.tisawesomeness.minecord.setting.parse.SettingCommandType}.
+ */
+package com.tisawesomeness.minecord.setting.parse;
