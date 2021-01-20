8af1d22f655b5f6d6551b185b48286b87636b2b8
Tisawesomeness <17386338+Tisawesomeness@users.noreply.github.com>
2021-01-20 17:02:51 -0500

    Rewrite and improve &skin

--- src/main/java/com/tisawesomeness/minecord/command/player/SkinCommand.java
+++ src/main/java/com/tisawesomeness/minecord/command/player/SkinCommand.java
@@ -1,67 +1,40 @@
 package com.tisawesomeness.minecord.command.player;
 
-import com.tisawesomeness.minecord.Bot;
 import com.tisawesomeness.minecord.command.CommandContext;
-import com.tisawesomeness.minecord.util.DateUtils;
-import com.tisawesomeness.minecord.util.NameUtils;
+import com.tisawesomeness.minecord.lang.Lang;
+import com.tisawesomeness.minecord.mc.player.Player;
 
 import lombok.NonNull;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.utils.MarkdownUtil;
 
-import java.util.Arrays;
-
-public class SkinCommand extends AbstractPlayerCommand {
+public class SkinCommand extends BasePlayerCommand {
 
     public @NonNull String getId() {
         return "skin";
     }
 
-    public void run(String[] args, CommandContext ctx) {
+    public void onSuccessfulPlayer(CommandContext ctx, Player player) {
+        String title = ctx.i18nf("title", player.getUsername());
+        String skinHistoryUrl = player.getMCSkinHistoryUrl().toString();
+        String avatarUrl = player.getAvatarUrl().toString();
+        String description = constructDescription(ctx, player);
 
-        //No arguments message
-        if (args.length == 0) {
-            ctx.showHelp();
-            return;
-        }
-
-        ctx.triggerCooldown();
-        String player = args[0];
-        String param = player;
-        if (!player.matches(NameUtils.uuidRegex)) {
-            String uuid = null;
-
-            //Parse date argument
-            if (args.length > 1) {
-                long timestamp = DateUtils.getTimestamp(Arrays.copyOfRange(args, 1, args.length));
-                if (timestamp == -1) {
-                    ctx.showHelp();
-                    return;
-                }
-
-            //Get the UUID
-                uuid = NameUtils.getUUID(player, timestamp);
-            } else {
-                uuid = NameUtils.getUUID(player);
-            }
-
-            //Check for errors
-            if (uuid == null) {
-                String m = "The Mojang API could not be reached." +
-                    "\n" + "Are you sure that username exists?" +
-                    "\n" + "Usernames are case-sensitive.";
-                ctx.possibleErr(m);
-                return;
-            } else if (!uuid.matches(NameUtils.uuidRegex)) {
-                ctx.err("The API responded with an error:\n" + uuid);
-                return;
-            }
-
-            param = uuid;
-        }
-
-        //Fetch skin
-        String url = "https://crafatar.com/skins/" + param.replaceAll("-", "");
-        ctx.replyRaw(new EmbedBuilder().setImage(url).setColor(Bot.color));
+        EmbedBuilder eb = new EmbedBuilder()
+                .setAuthor(title, skinHistoryUrl, avatarUrl)
+                .setDescription(description)
+                .setImage(player.getSkinUrl().toString());
+        ctx.reply(eb);
+    }
+    private static @NonNull String constructDescription(CommandContext ctx, Player player) {
+        Lang lang = ctx.getLang();
+        String custom = MarkdownUtil.bold(lang.i18n("mc.player.skin.custom")) + ": " +
+                lang.localize(player.hasCustomSkin());
+        String skinType = MarkdownUtil.bold(lang.i18n("mc.player.skin.model")) + ": " +
+                lang.i18n(player.getSkinType().getTranslationKey());
+        String defaultModel = MarkdownUtil.bold(lang.i18n("mc.player.skin.default")) + ": " +
+                lang.i18n(player.getDefaultSkinType().getTranslationKey());
+        return custom + "\n" + skinType + "\n" + defaultModel;
     }
 
 }
