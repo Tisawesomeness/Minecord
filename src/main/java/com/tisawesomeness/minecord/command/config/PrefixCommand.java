16f8761e74f8b634baaa269e7874e69e8ec19330
Tisawesomeness <17386338+Tisawesomeness@users.noreply.github.com>
2021-01-24 23:08:32 -0500

    Show &prefix in help

--- src/main/java/com/tisawesomeness/minecord/command/config/PrefixCommand.java
+++ src/main/java/com/tisawesomeness/minecord/command/config/PrefixCommand.java
@@ -1,12 +1,11 @@
 package com.tisawesomeness.minecord.command.config;
 
 import com.tisawesomeness.minecord.command.CommandContext;
-import com.tisawesomeness.minecord.command.IHiddenCommand;
 import com.tisawesomeness.minecord.setting.parse.SmartSetParser;
 
 import lombok.NonNull;
 
-public class PrefixCommand extends AbstractConfigCommand implements IHiddenCommand {
+public class PrefixCommand extends AbstractConfigCommand {
 
     public @NonNull String getId() {
         return "prefix";
