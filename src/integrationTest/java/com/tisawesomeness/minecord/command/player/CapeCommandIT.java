84180da0e1d1a83dd1b6f75d8ac61f673ac5239b
Tisawesomeness <17386338+Tisawesomeness@users.noreply.github.com>
2021-01-28 17:14:09 -0500

    Test &cape

---
+++ src/integrationTest/java/com/tisawesomeness/minecord/command/player/CapeCommandIT.java
@@ -0,0 +1,77 @@
+package com.tisawesomeness.minecord.command.player;
+
+import com.tisawesomeness.minecord.command.Result;
+import com.tisawesomeness.minecord.config.ConfigReader;
+import com.tisawesomeness.minecord.mc.player.Username;
+import com.tisawesomeness.minecord.testutil.mc.TestMCLibrary;
+import com.tisawesomeness.minecord.testutil.mc.TestPlayerProvider;
+import com.tisawesomeness.minecord.testutil.runner.TestCommandRunner;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.tisawesomeness.minecord.testutil.runner.CommandAssertions.assertThat;
+
+public class CapeCommandIT {
+
+    private static final Username THROWING_USERNAME = new Username("throw");
+    private static TestCommandRunner runner;
+
+    @BeforeAll
+    private static void initRunner() throws JsonProcessingException {
+        runner = new TestCommandRunner(ConfigReader.readFromResources(), new CapeCommand());
+        TestMCLibrary library = new TestMCLibrary();
+        TestPlayerProvider playerProvider = library.getPlayerProvider();
+        playerProvider.throwOnUsername(THROWING_USERNAME);
+        runner.mcLibrary = library;
+    }
+
+    @Test
+    @DisplayName("Cape command with no args only requests help")
+    public void testNoArgs() {
+        assertThat(runner.run()).onlyShowsHelp();
+    }
+
+    @Test
+    @DisplayName("Cape command with too long username warns the user")
+    public void testTooLong() {
+        String args = "A".repeat(Username.MAX_LENGTH + 1);
+        assertThat(runner.run(args))
+                .awaitResult()
+                .hasNotTriggeredCooldown()
+                .resultIs(Result.WARNING);
+    }
+
+    @Test
+    @DisplayName("Cape command with non-ascii username warns the user")
+    public void testNonAsciiUsername() {
+        String args = "ooθoo";
+        assertThat(runner.run(args))
+                .awaitResult()
+                .hasNotTriggeredCooldown()
+                .resultIs(Result.WARNING);
+    }
+
+    @Test
+    @DisplayName("Cape command responds with error when an IOE is thrown")
+    public void testIOE() {
+        String args = THROWING_USERNAME.toString();
+        assertThat(runner.run(args))
+                .awaitResult()
+                .resultIs(Result.ERROR)
+                .embedRepliesIsEmpty();
+    }
+
+    @Test
+    @DisplayName("Cape command responds with success, even though the username doesn't exist")
+    public void testNonExistentUsername() {
+        String args = "DoesNotExist";
+        assertThat(runner.run(args))
+                .awaitResult()
+                .hasTriggeredCooldown()
+                .isSuccess();
+    }
+
+}
