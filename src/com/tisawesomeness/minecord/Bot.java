264a328ff96dfe5b9510b0cf4adf2c1e5e6f7224
Tisawesomeness <17386338+Tisawesomeness@users.noreply.github.com>
2021-02-17 13:20:19 -0500

    Bump version to 0.12.4

--- src/com/tisawesomeness/minecord/Bot.java
+++ src/com/tisawesomeness/minecord/Bot.java
@@ -41,7 +41,7 @@ public class Bot {
 	public static final String helpServer = "https://minecord.github.io/support";
 	public static final String website = "https://minecord.github.io";
 	public static final String github = "https://github.com/Tisawesomeness/Minecord";
-	private static final String version = "0.12.3";
+	private static final String version = "0.12.4";
 	public static final String javaVersion = "1.8";
 	public static final String jdaVersion = "4.2.0_168";
 	public static final Color color = Color.GREEN;
