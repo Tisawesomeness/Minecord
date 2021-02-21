c98117e9e92cc846f6711ad1a401e00820242f8d
Tisawesomeness <17386338+Tisawesomeness@users.noreply.github.com>
2021-02-21 00:12:17 -0500

    Parse UUIDs from NBT formats

--- src/test/java/com/tisawesomeness/minecord/util/UUIDUtilsTest.java
+++ src/test/java/com/tisawesomeness/minecord/util/UUIDUtilsTest.java
@@ -3,10 +3,7 @@ package com.tisawesomeness.minecord.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.Arrays;
 import java.util.UUID;
@@ -14,6 +11,7 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class UUIDUtilsTest {
 
@@ -77,6 +75,13 @@ public class UUIDUtilsTest {
                 .map(UUIDUtils::toLongString)
                 .get().asString()
                 .isEqualToIgnoringCase(candidate);
+    }
+
+    @ParameterizedTest(name = "{index} ==> String {0} is parsed to UUID {1}")
+    @MethodSource("fromStringTestCaseProvider")
+    @DisplayName("fromString() parses all UUID formats")
+    public void testFromString(String candidate, UUID expected) {
+        assertThat(UUIDUtils.fromString(candidate)).contains(expected);
     }
 
     @ParameterizedTest(name = "{index} ==> String {0} is an invalid UUID")
@@ -172,6 +177,13 @@ public class UUIDUtilsTest {
     }
 
     @Test
+    @DisplayName("Int arrays are still correctly converted to UUIDs")
+    public void testFromInts() {
+        UUID uuid = UUIDUtils.fromInts(TESTING_ARRAY[0], TESTING_ARRAY[1], TESTING_ARRAY[2], TESTING_ARRAY[3]);
+        assertThat(uuid).isEqualTo(TESTING_UUID);
+    }
+
+    @Test
     @DisplayName("Int array NBT is generated correctly")
     public void testIntArrayString() {
         assertThat(UUIDUtils.toIntArrayString(TESTING_UUID))
@@ -180,6 +192,32 @@ public class UUIDUtilsTest {
 
     private static Stream<String> invalidUUIDProvider() {
         return Arrays.stream(invalidUuids);
+    }
+
+    private static Stream<Arguments> fromStringTestCaseProvider() {
+        return Stream.of(
+                a("[I;-1785359951,655379345,-1567137650,960850199]", "959591b1-2710-4b91-a297-608e39456917"),
+                a("[I;-2102862447,1879393870,-2014540146,125643402]", "82a8dd91-7005-464e-87ec-8e8e077d2a8a"),
+                a("[342853610,-932101771,-1703430175,500708399]", "146f87ea-c871-4175-9a77-b7e11dd8342f"),
+                a("[866073023,-1072676134,-1547803257,8928888]", "339f39bf-c010-42da-a3be-658700883e78"),
+                a("I;679758682,1685604387,-1826401735,785978687", "28844b5a-6478-4823-9323-52392ed9153f"),
+                a("I;-2110196118,-1581036440,-1548350330,705679212", "8238f66a-a1c3-4c68-a3b6-0c862a0fcf6c"),
+                a("-1984238450,1925729318,-1296717124,-1374087315", "89baec8e-72c8-4c26-b2b5-aabcae19176d"),
+                a("-577057643,383995047,-1924657720,-556447614", "dd9acc95-16e3-4ca7-8d48-0dc8ded54882"),
+                a("UUIDMost:8819294116821355910,UUIDLeast:-5680796684596543744", "7a646d45-cad4-4d86-b129-c1837567ff00"),
+                a("UUIDMost:-8535925615987506086,UUIDLeast:-8128030067067173204", "898a4ce0-a5e8-445a-8f33-6fb7d8c446ac"),
+                a("UUIDLeast:-6539246955979805517,UUIDMost:8956678450550099671", "7c4c8398-a46f-4ed7-a53f-ed8a3a44ecb3"),
+                a("UUIDLeast:-7535655018856453459,UUIDMost:5895376233818899421", "51d09573-1a0e-43dd-976b-f9af9b2beaad"),
+                a("Most:3023589638773621173,Least:-7713543321097612748", "29f5f2c1-bda7-41b5-94f3-fd37b263d234"),
+                a("Most:1440386925114507960,Least:-7954783195611741291", "13fd4846-e291-42b8-919a-eed47050c395"),
+                a("Least:-5126714713659541651,Most:-4763465308336599919", "bde4c58a-3cb3-4c91-b8da-4021638df76d"),
+                a("Least:-5447784046885623818,Most:3706888460998821717", "3371836a-53d4-4755-b465-9545e057a7f6"),
+                a("638511697757360091,-6677380810992916289", "08dc7301-91c2-4fdb-a355-2d87dac1e8bf"),
+                a("5540729442623441822,-7290368648222651612", "4ce4a012-701e-4b9e-9ad3-6857290b4b24")
+        );
+    }
+    private static Arguments a(String s, String u) {
+        return arguments(s, UUID.fromString(u));
     }
 
 }
