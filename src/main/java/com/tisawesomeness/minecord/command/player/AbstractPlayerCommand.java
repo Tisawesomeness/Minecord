3e358fcd32ebf09184172c970f2e22a33b5c57c4
Tisawesomeness <17386338+Tisawesomeness@users.noreply.github.com>
2021-02-26 22:45:54 -0500

    Patch last commit

--- src/main/java/com/tisawesomeness/minecord/command/player/AbstractPlayerCommand.java
+++ src/main/java/com/tisawesomeness/minecord/command/player/AbstractPlayerCommand.java
@@ -1,11 +1,27 @@
 package com.tisawesomeness.minecord.command.player;
 
 import com.tisawesomeness.minecord.command.Command;
+import com.tisawesomeness.minecord.command.CommandContext;
 import com.tisawesomeness.minecord.command.Module;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
 public abstract class AbstractPlayerCommand extends Command {
     @Override
     public final Module getModule() {
         return Module.PLAYER;
     }
+
+    protected static void handleIOE(Throwable ex, CommandContext ctx, String errorMessage) {
+        if (ex instanceof IOException) {
+            log.error(errorMessage, ex);
+            ctx.err(ctx.getLang().i18n("mc.external.mojang.error"));
+            return;
+        }
+        throw new RuntimeException(ex);
+    }
+
 }
