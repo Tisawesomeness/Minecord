d8064c94358ff096a4b1f6d44b3128fdae200a10
Tisawesomeness <17386338+Tisawesomeness@users.noreply.github.com>
2021-03-02 16:46:05 -0500

    Update &credits with recent changes

--- src/main/java/com/tisawesomeness/minecord/command/misc/CreditsCommand.java
+++ src/main/java/com/tisawesomeness/minecord/command/misc/CreditsCommand.java
@@ -14,18 +14,19 @@ public class CreditsCommand extends AbstractMiscCommand {
     }
 
     private final String DEVS = MarkdownUtil.maskedLink("Tis_awesomeness#8617", "https://github.com/Tisawesomeness") + " - Main Dev\n" +
-        MarkdownUtil.maskedLink("jbs#6969", "https://github.com/lordjbs") + " - Developer\n" +
-        MarkdownUtil.maskedLink("samueldcs#4675", "https://github.com/samueldcs") + " - Made the Website\n" +
-        MarkdownUtil.maskedLink("DJ Electro#1677", "https://github.com/Electromaster232") + " - Helped with Git once";
+            MarkdownUtil.maskedLink("jbs#6969", "https://github.com/lordjbs") + " - Developer\n" +
+            MarkdownUtil.maskedLink("samueldcs#4675", "https://github.com/samueldcs") + " - Made the Website\n" +
+            MarkdownUtil.maskedLink("DJ Electro#1677", "https://github.com/Electromaster232") + " - Supplied Hosting";
     
     private final String APIS = "Discord API Wrapper - " + MarkdownUtil.maskedLink("JDA", "https://github.com/DV8FromTheWorld/JDA") + "\n" +
-        "MC Account Info - " + MarkdownUtil.maskedLink("Mojang API", "https://wiki.vg/Mojang_API") + "\n" +
-        "Skin Renders - " + MarkdownUtil.maskedLink("Crafatar", "https://crafatar.com") + "\n" +
-        "Server Pinging - " + MarkdownUtil.maskedLink("MCServerPing", "https://github.com/lucaazalim/minecraft-server-ping") + "\n" +
-        "Cape Sites - " +
-            MarkdownUtil.maskedLink("Optifine", "https://optifine.net") + ", " +
-            MarkdownUtil.maskedLink("LabyMod", "https://www.labymod.net") + ", " +
-            MarkdownUtil.maskedLink("MinecraftCapes", "https://www.minecraftcapes.co.uk");
+            "MC Account Info - " +
+                    MarkdownUtil.maskedLink("Mojang API", "https://wiki.vg/Mojang_API") + ", " +
+                    MarkdownUtil.maskedLink("Electroid API", "https://github.com/Electroid/mojang-api") + "\n" +
+            "Skin Renders - " + MarkdownUtil.maskedLink("Crafatar", "https://crafatar.com") + "\n" +
+            "Server Pinging - " + MarkdownUtil.maskedLink("MCServerPing", "https://github.com/lucaazalim/minecraft-server-ping") + "\n" +
+            "Optifine Capes - " + MarkdownUtil.maskedLink("Optifine", "https://optifine.net");
+
+    private final String HOST = "The public bot is proudly hosted by " + MarkdownUtil.maskedLink("Endless Hosting", "https://theendlessweb.com/");
 
     public void run(String[] args, CommandContext ctx) {
         ctx.triggerCooldown();
@@ -35,20 +36,24 @@ public class CreditsCommand extends AbstractMiscCommand {
                 MarkdownUtil.maskedLink("Suggest features here", branding.getHelpServer());
 
         EmbedBuilder eb = new EmbedBuilder()
-            .setTitle("Minecord Credits")
-            .setColor(branding.getColor())
-            .setDescription("Thanks to all these great people who helped to make the bot possible. :heart:")
-            .addField("Developers", DEVS, true)
-            .addField("Contribute", contrib, true)
-            .addField("APIs Used", APIS, false);
+                .setTitle("Minecord Credits")
+                .setColor(branding.getColor())
+                .setDescription("Thanks to all these great people who helped make the bot possible. :heart:")
+                .addField("Developers", DEVS, true)
+                .addField("Contribute", contrib, true)
+                .addField("APIs Used", APIS, false);
         if (ctx.getConfig().isSelfHosted()) {
             String selfHost = "This bot is self-hosted by **" + branding.getAuthor() + "**\n" +
                     "Original Website - " + MarkdownUtil.maskedLink(Branding.WEBSITE, Branding.WEBSITE) + "\n" +
                     "Original Help Server - " + MarkdownUtil.maskedLink(Branding.HELP_SERVER, Branding.HELP_SERVER) + "\n" +
                     "Original Github - " + MarkdownUtil.maskedLink("Source Code", Branding.GITHUB);
             eb.addField("Self-Hosting", selfHost, false);
+        } else {
+            eb.addField("Hosting", HOST, false);
         }
-        eb.setFooter("Website: " + branding.getWebsite());
+        String userMention = ctx.getE().getAuthor().getAsMention();
+        eb.addField("Special Thanks", userMention + " for using Minecord!", false)
+                .setFooter("Website - " + branding.getWebsite());
 
         ctx.replyRaw(eb);
     }
