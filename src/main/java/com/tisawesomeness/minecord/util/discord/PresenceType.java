10e48d56f7c456b235ee349cc19aa2d8fc66438b
Tisawesomeness <17386338+Tisawesomeness@users.noreply.github.com>
2021-05-09 21:39:22 -0400

    Add competing presence

--- src/main/java/com/tisawesomeness/minecord/util/discord/PresenceType.java
+++ src/main/java/com/tisawesomeness/minecord/util/discord/PresenceType.java
@@ -11,7 +11,8 @@ import net.dv8tion.jda.api.entities.Activity;
 public enum PresenceType {
     PLAYING(Activity.ActivityType.DEFAULT),
     STREAMING(Activity.ActivityType.STREAMING),
-    LISTENING(Activity.ActivityType.LISTENING);
+    LISTENING(Activity.ActivityType.LISTENING),
+    COMPETING(Activity.ActivityType.COMPETING);
 
     @Getter private final Activity.ActivityType activityType;
 }
