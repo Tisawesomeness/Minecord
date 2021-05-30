c10086c52e34ec3d52af87becef4b11f0af5de4a
Tisawesomeness <tis_dev@protonmail.com>
2021-05-30 09:21:14 -0500

    Test placeholders

---
+++ src/test/java/com/tisawesomeness/minecord/PlaceholdersTest.java
@@ -0,0 +1,71 @@
+package com.tisawesomeness.minecord;
+
+import com.tisawesomeness.minecord.config.config.Config;
+import com.tisawesomeness.minecord.testutil.Resources;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class PlaceholdersTest {
+
+    private static Config config;
+    private static BotBranding branding = new BotBranding();
+    private static BuildInfo buildInfo = BuildInfo.getInstance();
+
+    @BeforeAll
+    private static void initConfig() throws JsonProcessingException {
+        config = Resources.config();
+    }
+
+    @ParameterizedTest(name = "{index} ==> Placeholder {0} is replaced with {1}")
+    @DisplayName("Placeholders by themselves are replaced")
+    @MethodSource("placeholderProvider")
+    public void testPlaceholders(String placeholder, String value) {
+        assertThat(Placeholders.parseConstants(placeholder, config, branding)).isEqualTo(value);
+    }
+    @ParameterizedTest(name = "{index} ==> Placeholder {0} is replaced with {1}")
+    @DisplayName("Placeholders surrounded by other characters are replaced")
+    @MethodSource("placeholderProvider")
+    public void testPlaceholdersWithExtra(String placeholder, String value) {
+        String input = ">" + placeholder + "<";
+        String output = ">" + value + "<";
+        assertThat(Placeholders.parseConstants(input, config, branding)).isEqualTo(output);
+    }
+    @Test
+    @DisplayName("Multiple placeholders can be replaced in the same string")
+    public void testMultiplePlaceholders() {
+        String input = Placeholders.PREFIX + "user " + Placeholders.AUTHOR_TAG;
+        String output = config.getSettingsConfig().getDefaultPrefix() + "user " + branding.getAuthorTag();
+        assertThat(Placeholders.parseConstants(input, config, branding)).isEqualTo(output);
+    }
+
+    private static Stream<Arguments> placeholderProvider() {
+        return Stream.of(
+                arguments(Placeholders.AUTHOR, branding.getAuthor()),
+                arguments(Placeholders.AUTHOR_TAG, branding.getAuthorTag()),
+                arguments(Placeholders.INVITE, branding.getInvite()),
+                arguments(Placeholders.HELP_SERVER, branding.getHelpServer()),
+                arguments(Placeholders.WEBSITE, branding.getWebsite()),
+                arguments(Placeholders.GITHUB, branding.getGithub()),
+
+                arguments(Placeholders.VERSION, buildInfo.version),
+                arguments(Placeholders.JDA_VERSION, buildInfo.jdaVersion),
+
+                arguments(Placeholders.JAVA_VERSION, System.getProperty("java.version")),
+                arguments(Placeholders.MC_VERSION, config.getSupportedMCVersion()),
+                arguments(Placeholders.BOT_SHARDS, String.valueOf(config.getShardCount())),
+                arguments(Placeholders.PREFIX, config.getSettingsConfig().getDefaultPrefix())
+        );
+    }
+
+}
