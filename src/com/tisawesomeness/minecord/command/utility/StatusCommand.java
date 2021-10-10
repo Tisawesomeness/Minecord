15ff63de32c19b3ab81170b4f1ba25283f2c9d4d
Tisawesomeness <tis_dev@protonmail.com>
2021-10-10 18:29:12 -0400

    Ping status servers directly

--- src/com/tisawesomeness/minecord/command/utility/StatusCommand.java
+++ src/com/tisawesomeness/minecord/command/utility/StatusCommand.java
@@ -7,11 +7,22 @@ import com.tisawesomeness.minecord.util.RequestUtils;
 
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import org.json.JSONObject;
 
 import java.awt.Color;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class StatusCommand extends Command {
+
+	private static final List<String> URLS = Arrays.asList(
+			"https://minecraft.net",
+			"https://account.mojang.com",
+			"https://authserver.mojang.com",
+			"https://textures.minecraft.net",
+			"https://api.mojang.com"
+	);
 	
 	public CommandInfo getInfo() {
 		return new CommandInfo(
@@ -27,36 +38,42 @@ public class StatusCommand extends Command {
 	}
 	
 	public Result run(String[] args, MessageReceivedEvent e) {
-		// Request information from Mojang through Obsidion API
-		String request = RequestUtils.get("https://api.obsidion-dev.com/api/v1/mojang/check", null, true);
-		if (request == null) {
-			return new Result(Outcome.ERROR, ":x: The Mojang API could not be checked.");
-		}
+		// Pings done in separate threads to speed up in case some URLs timeout
+		List<CompletableFuture<Boolean>> statusRequests = URLS.stream()
+				.map(StatusCommand::checkUrl)
+				.collect(Collectors.toList());
+		CompletableFuture.allOf(statusRequests.toArray(new CompletableFuture[URLS.size()])).join();
+		List<Boolean> statuses = statusRequests.stream()
+				.map(CompletableFuture::join)
+				.collect(Collectors.toList());
 
-		// Extract statuses
-		JSONObject status = new JSONObject(request);
-		boolean mcStatus = isGreen(status, "https://www.minecraft.net");
-		boolean accountStatus = isGreen(status, "https://account.mojang.com");
-		boolean authStatus = isGreen(status, "https://authserver.mojang.com");
-		boolean textureStatus = isGreen(status, "https://textures.minecraft.net");
-		boolean apiStatus = isGreen(status, "https://api.mojang.com");
+		// Transform list of true/false into emote check/x
+		List<String> statusEmotes = statuses.stream()
+				.map(DiscordUtils::getBoolEmote)
+				.collect(Collectors.toList());
 
-		boolean allGood = mcStatus && accountStatus && authStatus && textureStatus && apiStatus;
+		String m = "**Minecraft:** " + statusEmotes.get(0) +
+				"\n" + "**Accounts:** " + statusEmotes.get(1) +
+				"\n" + "**Auth Server:** " + statusEmotes.get(2) +
+				"\n" + "**Textures:** " + statusEmotes.get(3) +
+				"\n" + "**Mojang API:** " + statusEmotes.get(4);
+
+		boolean allGood = statuses.stream().allMatch(b -> b);
 		Color color = allGood ? Color.GREEN : Color.RED;
-
-		// Build message
-		String m = "**Minecraft:** " + DiscordUtils.getBoolEmote(mcStatus) +
-				"\n" + "**Accounts:** " + DiscordUtils.getBoolEmote(accountStatus) +
-				"\n" + "**Auth Server:** " + DiscordUtils.getBoolEmote(authStatus) +
-				"\n" + "**Textures:** " + DiscordUtils.getBoolEmote(textureStatus) +
-				"\n" + "**Mojang API:** " + DiscordUtils.getBoolEmote(apiStatus);
 		
 		MessageEmbed me = MessageUtils.embedMessage("Minecraft Status", null, m, color);
 		return new Result(Outcome.SUCCESS, me);
 	}
 
-	private static boolean isGreen(JSONObject status, String key) {
-		return "green".equals(status.optString(key));
+	private static CompletableFuture<Boolean> checkUrl(String url) {
+		return CompletableFuture.supplyAsync(() -> check(url));
+	}
+	private static boolean check(String url) {
+		// The Minecraft website likes to bug out for some reason, regular GET request sometimes breaks
+		if (url.equals("https://minecraft.net")) {
+			return RequestUtils.checkWithSocket("minecraft.net");
+		}
+		return RequestUtils.checkURLWithGet(url);
 	}
 	
 }
