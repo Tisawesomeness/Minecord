084414fdb4d5aeffb6c0e74debdb94f5d050ed6d
Spongecade <spongecade.129@gmail.com>
2023-09-27 18:30:14 +0000

    Update Minecraft Wiki references to new domain

--- minecord-bot/src/main/java/com/tisawesomeness/minecord/command/utility/CodesCommand.java
+++ minecord-bot/src/main/java/com/tisawesomeness/minecord/command/utility/CodesCommand.java
@@ -12,7 +12,7 @@ public class CodesCommand extends AbstractUtilityCommand {
         return "codes";
     }
 
-    private String img = "https://minecraft.gamepedia.com/media/minecraft.gamepedia.com/7/7e/Minecraft_Formatting.gif";
+    private String img = "https://minecraft.wiki/images/Minecraft_Formatting.gif?2311f";
 
     public void run(String[] args, CommandContext ctx) {
 
