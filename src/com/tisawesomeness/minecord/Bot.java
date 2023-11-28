551e61fb2b3da553bd47fac62ba86e49a3ad767e
Tisawesomeness <tis_dev@protonmail.com>
2023-11-28 00:58:56 -0500

    Mov "bot started" from status to bot log

--- src/com/tisawesomeness/minecord/Bot.java
+++ src/com/tisawesomeness/minecord/Bot.java
@@ -237,7 +237,7 @@ public class Bot {
         shardManager.retrieveUserById(Config.getOwner()).queue(u -> ownerAvatarUrl = u.getAvatarUrl());
         bootTime = System.currentTimeMillis() - birth;
         System.out.println("Boot Time: " + DateUtils.getBootTime());
-        logger.statusLog(":white_check_mark: **Bot started!**");
+        logger.log(":white_check_mark: **Bot started!**");
         DiscordUtils.update();
         RequestUtils.sendGuilds();
 
