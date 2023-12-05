636c6470726b93d5d51a0fef98fbe10a4a4a0c86
Tisawesomeness <tis_dev@protonmail.com>
2023-12-05 12:31:48 -0500

    Updated dependencies

--- src/com/tisawesomeness/minecord/Bot.java
+++ src/com/tisawesomeness/minecord/Bot.java
@@ -45,7 +45,7 @@ public class Bot {
     public static final String terms = "https://minecord.github.io/terms";
     public static final String privacy = "https://minecord.github.io/privacy";
     private static final String version = "0.17.3";
-    public static final String jdaVersion = "5.0.0-beta.17";
+    public static final String jdaVersion = "5.0.0-beta.18";
     public static final Color color = Color.GREEN;
 
     public static ShardManager shardManager;
