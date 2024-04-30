8118d11388e9f0eef956f09b7a9553138e3a00c1
Tisawesomeness <tis_dev@protonmail.com>
2024-04-29 21:31:31 -0400

    Update default supported version to 1.20.6

--- src/com/tisawesomeness/minecord/Config.java
+++ src/com/tisawesomeness/minecord/Config.java
@@ -121,7 +121,7 @@ public class Config {
             logWebhook = settings.optString("logWebhook", "");
             statusWebhook = settings.optString("statusWebhook", "");
             includeSpamStatuses = settings.optBoolean("includeSpamStatuses", false);
-            supportedMCVersion = settings.optString("supportedMCVersion", "1.20.3");
+            supportedMCVersion = settings.optString("supportedMCVersion", "1.20.6");
             isSelfHosted = settings.optBoolean("isSelfHosted", true);
             if (isSelfHosted) {
                 author = settings.getString("author");
