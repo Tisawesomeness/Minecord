62720b95b2a0a33aa613a44baf978bc6e2252718
Tisawesomeness <tis_dev@protonmail.com>
2025-12-02 00:47:34 -0500

    Add @Minecord log

--- src/com/tisawesomeness/minecord/command/Registry.java
+++ src/com/tisawesomeness/minecord/command/Registry.java
@@ -101,6 +101,7 @@ public class Registry {
                        new ShutdownCommand(),
                        new DeployCommand(),
                        new EvalCommand(),
+                       new LogCommand(),
                        new TestCommand()
             )
     };
