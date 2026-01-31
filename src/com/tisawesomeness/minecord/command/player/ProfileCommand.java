7d5d2d5a8850943c28c6bbd6ddbb7b20cefaa430
Tisawesomeness <tis_dev@protonmail.com>
2026-01-31 18:32:15 -0500

    Remove history warning from /profile

--- src/com/tisawesomeness/minecord/command/player/ProfileCommand.java
+++ src/com/tisawesomeness/minecord/command/player/ProfileCommand.java
@@ -93,8 +93,6 @@ public class ProfileCommand extends BasePlayerCommand {
                     .addField("Account", accountInfo, true);
         }
 
-        String nameHistory = "Mojang has removed the name history API, [read more here](https://help.minecraft.net/hc/en-us/articles/8969841895693). We are working on a different way to retrieve name history, stay tuned.";
-        eb.addField("Name History", nameHistory, true);
         uploadOrEmbedImages(e, eb.build());
     }
 
