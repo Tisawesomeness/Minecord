8cc7dd41ea145ba24d0f4b3a8f83a75954d1ba08
Tisawesomeness <tis_dev@protonmail.com>
2026-07-23 00:24:57 -0400

    Bump version to 0.19.0

--- src/com/tisawesomeness/minecord/Bot.java
+++ src/com/tisawesomeness/minecord/Bot.java
@@ -60,7 +60,7 @@ public class Bot {
     public static final String donate = "https://ko-fi.com/tis_awesomeness";
     public static final String terms = "https://minecord.github.io/terms";
     public static final String privacy = "https://minecord.github.io/privacy";
-    public static final String version = "0.18.10";
+    public static final String version = "0.19.0";
     public static final String jdaVersion = "6.5.0";
     public static final Color color = Color.GREEN;
 
