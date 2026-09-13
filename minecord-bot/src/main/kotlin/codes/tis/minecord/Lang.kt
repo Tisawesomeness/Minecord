82185ac53015babc00f1cdaa5349d91080be0651
Tisawesomeness <tis_dev@protonmail.com>
2026-09-13 17:36:31 -0400

    feat: lang MessageFormat

--- minecord-bot/src/main/kotlin/codes/tis/minecord/Lang.kt
+++ minecord-bot/src/main/kotlin/codes/tis/minecord/Lang.kt
@@ -1,15 +1,66 @@
 package codes.tis.minecord
 
 import org.jetbrains.annotations.PropertyKey
+import java.text.MessageFormat
+import java.util.Locale
 import java.util.ResourceBundle
 
-private const val baseName = "lang.lang"
-private val bundle: ResourceBundle = ResourceBundle.getBundle(baseName)
+private const val BASE_NAME = "lang.lang"
+private val bundle: ResourceBundle = ResourceBundle.getBundle(BASE_NAME)
+private val locale = Locale.ENGLISH
 
-fun localize(key: TranslationKey): String = bundle.getString(key.key)
 fun localize(key: String): String? = if (bundle.containsKey(key)) bundle.getString(key) else null
 
+fun localize(key: TranslationKey): String = bundle.getString(key.key)
+
+fun localize(localizable: Localizable): String {
+    return MessageFormat(localize(localizable.key()), locale).format(localizable.args())
+}
+
+interface Localizable {
+    fun key(): TranslationKey
+
+    fun args(): Array<out Any> = emptyArray()
+}
+
+@Suppress("ktlint:standard:annotation")
 @JvmInline
-value class TranslationKey(val key: @PropertyKey(resourceBundle = baseName) String) {
+value class TranslationKey(val key: @PropertyKey(resourceBundle = BASE_NAME) String) : Localizable {
+    override fun key() = this
+
+    fun format(vararg args: Any) = TranslationFormat(this, *args)
+
     override fun toString() = key
+}
+
+/**
+ * A translation with arguments. The number of arguments is **NOT** validated.
+ * @throws IllegalArgumentException if one of the arguments is an array
+ */
+class TranslationFormat(val key: TranslationKey, vararg val args: Any) : Localizable {
+    init {
+        args.forEach { arg ->
+            require(arg !is Array<*>) { "Passing an array to `vararg args: Any` is probably a bug, use spread operator" }
+        }
+    }
+
+    override fun key() = key
+
+    override fun args() = args
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is TranslationFormat) return false
+
+        if (key != other.key) return false
+        if (!args.contentEquals(other.args)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = key.hashCode()
+        result = 31 * result + args.contentHashCode()
+        return result
+    }
 }
--- minecord-bot/src/test/kotlin/codes/tis/minecord/LangTest.kt
+++ minecord-bot/src/test/kotlin/codes/tis/minecord/LangTest.kt
@@ -1,18 +1,63 @@
 package codes.tis.minecord
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.equals.shouldEqual
+import io.kotest.matchers.equals.shouldNotEqual
 
 class LangTest : FunSpec({
-    test("localize returns localized string") {
-        localize(TranslationKey("minecord.generic.true")) shouldEqual "True"
+    context("localize") {
+        test("localize returns localized string") {
+            localize(TranslationKey("minecord.generic.true")) shouldEqual "True"
+        }
+
+        test("localize with raw string returns localized string") {
+            localize("minecord.generic.false") shouldEqual "False"
+        }
+
+        test("localize with raw string returns empty if not present") {
+            localize("minecord.definitely.does.not.exist123") shouldEqual null
+        }
+
+        test("localize with translation format returns formatted string") {
+            val localized = localize(TranslationKey("minecord.error.invalid_long").format("arg", "value"))
+            localized shouldEqual "`arg` must be an integer but was `value`"
+        }
     }
 
-    test("localize with raw string returns localized string") {
-        localize("minecord.generic.false") shouldEqual "False"
-    }
+    context("TranslationFormat") {
+        test("key returns the translation key") {
+            val format = TranslationKey("minecord.generic.true").format()
+            format.key() shouldEqual TranslationKey("minecord.generic.true")
+        }
 
-    test("localize with raw string returns empty if not present") {
-        localize("minecord.definitely.does.not.exist123") shouldEqual null
+        test("args returns the translation arguments") {
+            val format = TranslationKey("minecord.generic.true").format("yes", 123)
+            format.args().toList() shouldEqual listOf("yes", 123)
+        }
+
+        test("equals returns true for identical key and args") {
+            val format1 = TranslationKey("minecord.generic.true").format("yes", 123)
+            val format2 = TranslationKey("minecord.generic.true").format("yes", 123)
+            format1 shouldEqual format2
+        }
+
+        test("equals returns false when key differs") {
+            val format1 = TranslationKey("minecord.generic.true").format("yes", 123)
+            val format2 = TranslationKey("minecord.generic.false").format("no", 456)
+            format1 shouldNotEqual format2
+        }
+
+        test("equals returns false when args differ") {
+            val format1 = TranslationKey("minecord.generic.true").format("yes", 123)
+            val format2 = TranslationKey("minecord.generic.true").format("no", 456)
+            format1 shouldNotEqual format2
+        }
+
+        test("initializer throws IllegalArgumentException for Array arg") {
+            shouldThrow<IllegalArgumentException> {
+                TranslationKey("minecord.error.invalid_long").format(arrayOf(1, 2))
+            }
+        }
     }
 })
